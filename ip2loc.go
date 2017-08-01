c6a070d22c6a93957519b2aaf687ad2617f09dc5
Zhanliang Liu <zhanliangliu@qq.com>
2017-08-01 17:28:46 +0800

    ip address to country code mapping

---
+++ ip2loc.go
@@ -0,0 +1,53 @@
+package ip2loc
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"net"
+)
+
+var (
+	ErrInvalidIp  = errors.New("invalid ip address")
+	ErrIpNotFound = errors.New("ip address not found")
+)
+
+type IpRange struct {
+	Begin uint32
+	End   uint32
+	Data  string
+}
+
+func Find(ip string) (string, error) {
+	p, err := StrToInt(ip)
+	if err != nil {
+		return "", err
+	}
+	var low, high uint32 = 0, uint32(len(ipData) - 1)
+	for low <= high {
+		m := (low + high) / 2
+		if p >= ipData[m].Begin && p <= ipData[m].End {
+			return ipData[m].Data, nil
+		}
+		if p < ipData[m].Begin {
+			high = m
+		} else {
+			low = m
+		}
+	}
+	return "", ErrIpNotFound
+}
+
+func StrToInt(ip string) (uint32, error) {
+	netIP := net.ParseIP(ip)
+	if netIP == nil {
+		return 0, ErrInvalidIp
+	}
+	ipv4 := netIP.To4()
+	if ipv4 == nil {
+		return 0, ErrInvalidIp
+	}
+	var long uint32
+	binary.Read(bytes.NewBuffer(ipv4), binary.BigEndian, &long)
+	return long, nil
+}
